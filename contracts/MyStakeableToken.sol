555b12cef5882c1071bb023539c43668ad780a33
andreitoma8 <andrei.toma8@yahoo.com>
2022-03-18 17:00:26 +0200

    main

--- contracts/MyStakeableToken.sol
+++ contracts/MyStakeableToken.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.4;
 
 import "./ERC20Stakeable.sol";
 
-contract MyToken is ERC20Stakeable {
+contract MyStakeableToken is ERC20Stakeable {
     constructor(string memory _name, string memory _symbol)
         ERC20Stakeable(_name, _symbol)
     {
-        _mint(1000000, msg.sender);
+        _mint(msg.sender, 1000000);
     }
 }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,8 @@
-from brownie import MyToken, accounts, config
+from brownie import MyStakeableToken, accounts, config
 
 
 def main():
     account = accounts.add(config["wallets"]["from_key"])
-    my_token = MyToken.deploy(
+    my_token = MyStakeableToken.deploy(
         "MyStakeableToken", "MST", {"from": account}, publish_source=True
     )
