94b191ab461b56e9001097b9b516b42b56d6ff21
andreitoma8 <andrei.toma8@yahoo.com>
2022-03-21 18:04:46 +0200

    main

--- contracts/MyStakeableToken.sol
+++ contracts/MyStakeableToken.sol
@@ -11,19 +11,19 @@ contract MyStakeableToken is ERC20Stakeable, Ownable {
         _mint(msg.sender, 1000000 * 10**decimals());
     }
 
-    // Functions for staking mechanism variables:
+    // Functions for modifying  staking mechanism variables:
 
-    // Set rewards per hour as x/10.000.000 (Example: 100000 = 1%)
+    // Set rewards per hour as x/10.000.000 (Example: 100.000 = 1%)
     function setRewards(uint256 _rewardsPerHour) public onlyOwner {
         rewardsPerHour = _rewardsPerHour;
     }
 
-    // Set the minimum amount for stakeing in wei
+    // Set the minimum amount for staking in wei
     function setMinStake(uint256 _minStake) public onlyOwner {
         minStake = _minStake;
     }
 
-    // Set the minimum time that has to pass
+    // Set the minimum time that has to pass for a user to be able to restake rewards
     function setCompFreq(uint256 _compoundFreq) public onlyOwner {
         compoundFreq = _compoundFreq;
     }
